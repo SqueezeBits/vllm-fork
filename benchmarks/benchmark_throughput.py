70b0440cf5b1494a33657356aeeee6a8920d241a
JonghoLee <jongho.lee@squeezebits.com>
2024-07-02 07:53:14 +0000

    fix: keep the unused methods

--- benchmarks/benchmark_throughput.py
+++ benchmarks/benchmark_throughput.py
@@ -15,7 +15,7 @@ from transformers import (AutoModelForCausalLM, AutoTokenizer,
 from vllm.model_executor.layers.quantization import QUANTIZATION_METHODS
 
 
-def sample_conversations_requests(
+def sample_requests(
     dataset_path: str,
     num_requests: int,
     tokenizer: PreTrainedTokenizerBase,
@@ -60,7 +60,7 @@ def sample_conversations_requests(
 
     return filtered_dataset
 
-def sample_requests(
+def sample_requests_for_benchmark(
     dataset_path: str,
     tokenizer: PreTrainedTokenizerBase,
     num_requests: int=1024,
@@ -164,6 +164,85 @@ def run_vllm(
     elapsed_time = end - start
     return outputs, elapsed_time
 
+
+def run_hf(
+    requests: List[Tuple[str, int, int]],
+    model: str,
+    tokenizer: PreTrainedTokenizerBase,
+    n: int,
+    use_beam_search: bool,
+    max_batch_size: int,
+    trust_remote_code: bool,
+) -> float:
+    assert not use_beam_search
+    llm = AutoModelForCausalLM.from_pretrained(
+        model, torch_dtype=torch.float16, trust_remote_code=trust_remote_code)
+    if llm.config.model_type == "llama":
+        # To enable padding in the HF backend.
+        tokenizer.pad_token = tokenizer.eos_token
+    llm = llm.cuda()
+
+    pbar = tqdm(total=len(requests))
+    start = time.perf_counter()
+    batch: List[str] = []
+    max_prompt_len = 0
+    max_output_len = 0
+    for i in range(len(requests)):
+        prompt, prompt_len, output_len = requests[i]
+        # Add the prompt to the batch.
+        batch.append(prompt)
+        max_prompt_len = max(max_prompt_len, prompt_len)
+        max_output_len = max(max_output_len, output_len)
+        if len(batch) < max_batch_size and i != len(requests) - 1:
+            # Check if we can add more requests to the batch.
+            _, next_prompt_len, next_output_len = requests[i + 1]
+            if (max(max_prompt_len, next_prompt_len) +
+                    max(max_output_len, next_output_len)) <= 2048:
+                # We can add more requests to the batch.
+                continue
+
+        # Generate the sequences.
+        input_ids = tokenizer(batch, return_tensors="pt",
+                              padding=True).input_ids
+        llm_outputs = llm.generate(
+            input_ids=input_ids.cuda(),
+            do_sample=not use_beam_search,
+            num_return_sequences=n,
+            temperature=1.0,
+            top_p=1.0,
+            use_cache=True,
+            max_new_tokens=max_output_len,
+        )
+        # Include the decoding time.
+        tokenizer.batch_decode(llm_outputs, skip_special_tokens=True)
+        pbar.update(len(batch))
+
+        # Clear the batch.
+        batch = []
+        max_prompt_len = 0
+        max_output_len = 0
+    end = time.perf_counter()
+    return end - start
+
+
+def run_mii(
+    requests: List[Tuple[str, int, int]],
+    model: str,
+    tensor_parallel_size: int,
+    output_len: int,
+) -> float:
+    from mii import client, serve
+    llm = serve(model, tensor_parallel=tensor_parallel_size)
+    prompts = [prompt for prompt, _, _ in requests]
+
+    start = time.perf_counter()
+    llm.generate(prompts, max_new_tokens=output_len)
+    end = time.perf_counter()
+    client = client(model)
+    client.terminate_server()
+    return end - start
+
+
 def main(args: argparse.Namespace):
     print(args)
     
@@ -174,7 +253,7 @@ def main(args: argparse.Namespace):
     # Sample the requests.
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer, trust_remote_code=args.trust_remote_code)
     
-    requests = sample_requests(
+    requests = sample_requests_for_benchmark(
         args.dataset, 
         tokenizer, 
         args.num_prompts, 
@@ -182,16 +261,27 @@ def main(args: argparse.Namespace):
         args.output_len, 
         args.use_text_input)            
 
-    outputs, elapsed_time = run_vllm(
-        requests, args.model, args.tokenizer, args.quantization,
-        args.tensor_parallel_size, args.seed, args.n, args.use_beam_search,
-        args.trust_remote_code, args.dtype, args.max_model_len,
-        args.enforce_eager, args.kv_cache_dtype,
-        args.quantization_param_path, args.device,
-        args.enable_prefix_caching, args.enable_chunked_prefill,
-        args.max_num_batched_tokens, args.gpu_memory_utilization,
-        args.download_dir, args.enable_1d_query, args.enable_piggybacking, 
-        args.use_text_input, args.print_outputs)
+    if args.backend == "vllm":
+        outputs, elapsed_time = run_vllm(
+            requests, args.model, args.tokenizer, args.quantization,
+            args.tensor_parallel_size, args.seed, args.n, args.use_beam_search,
+            args.trust_remote_code, args.dtype, args.max_model_len,
+            args.enforce_eager, args.kv_cache_dtype,
+            args.quantization_param_path, args.device,
+            args.enable_prefix_caching, args.enable_chunked_prefill,
+            args.max_num_batched_tokens, args.gpu_memory_utilization,
+            args.download_dir, args.enable_1d_query, args.enable_piggybacking, 
+            args.use_text_input, args.print_outputs)
+    elif args.backend == "hf":
+        assert args.tensor_parallel_size == 1
+        elapsed_time = run_hf(requests, args.model, tokenizer, args.n,
+                              args.use_beam_search, args.hf_max_batch_size,
+                              args.trust_remote_code)
+    elif args.backend == "mii":
+        elapsed_time = run_mii(requests, args.model, args.tensor_parallel_size,
+                               args.output_len)
+    else:
+        raise ValueError(f"Unknown backend: {args.backend}")
 
     total_input_tokens = sum(input_len for _, input_len, _ in requests)
     total_output_tokens = [len(outputs[i].outputs[0].token_ids) for i in range(len(outputs))]
